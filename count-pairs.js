e786ec532726cdf7e440e6830df9c640dd8d3196
David Ames <readytowin2298@gmail.com>
2021-09-12 21:54:57 -0500

    countPairs

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(nums, sum) {
+    let seen = new Set();
+    let pairs = 0;
+
+    for(let num of nums){
+        if(seen.has(num)){
+            pairs++
+        } else {
+            seen.add(sum - num)
+        }
+    }
+
+    return pairs;
 }
+
+module.exports = countPairs;
