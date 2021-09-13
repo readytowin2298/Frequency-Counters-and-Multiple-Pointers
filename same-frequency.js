f15348da7f917d2cc3b95650be1cecc5a8d54ddb
David Ames <readytowin2298@gmail.com>
2021-09-12 20:59:36 -0500

    same-frequency

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function sameFrequency() {
+function sameFrequency(num1, num2) {
+
+    let arr1 = [...String(num1)].sort();
+    let arr2 = [...String(num2)].sort();
+
+    if(arr1.length != arr2.length) return false;
+
+    for(let i=0; i<arr1.length; i++){
+        if(arr1[i] !== arr2[i]){
+            return false;
+        }
+    }
+    return true;
+    
 }
+
+module.exports = sameFrequency;
