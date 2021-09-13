6e1d70cdf88c3aea3e114ba42799f824be288054
David Ames <readytowin2298@gmail.com>
2021-09-12 20:30:40 -0500

    two-array-object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,14 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, vals) {
+    let obj = {};
+
+    for(let i=0; i<keys.length; i++){
+        let key = keys[i];
+        let val = vals.length>i ? vals[i] : null;
+        obj[key] = val;
+    }
+    return obj;
+
 }
+
+module.exports = twoArrayObject;
