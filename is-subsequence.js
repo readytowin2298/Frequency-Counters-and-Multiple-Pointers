efbe7a8b50adf4274514a3bf886e2ce1b3f0b4f1
David Ames <readytowin2298@gmail.com>
2021-09-12 21:49:23 -0500

    isSubsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,12 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(sub, str) {
+
+    for(let i=0; i<sub.length; i++){
+        let idx = str.indexOf(sub[i]);
+        if(idx === -1) return false;
+        str = str.slice(idx)
+    }
+    return true;
 }
+
+module.exports = isSubsequence;
