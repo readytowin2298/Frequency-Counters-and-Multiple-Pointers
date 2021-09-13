dc7cf850c4eb3de2b22579a3b48cd391f16f60d2
David Ames <readytowin2298@gmail.com>
2021-09-12 20:22:36 -0500

    averagePair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,15 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(ints, target) {
+
+    const goal = new Set();
+    for(let int of ints){
+        if(goal.has(int)){
+            return true
+        }
+        let test = (target * 2) - int;
+        goal.add(test)
+    }
+    return false;
 }
+
+module.exports = averagePair
